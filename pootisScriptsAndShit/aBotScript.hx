83b7b6c30de7af642d38aa45097dafba4a66549b
BobbyDrawz_ <veersparekh@outlook.com>
2024-09-17 20:43:48 -0400

    Update aBotScript.hx

--- pootisScriptsAndShit/aBotScript.hx
+++ pootisScriptsAndShit/aBotScript.hx
@@ -4,6 +4,8 @@ var abot:ABotSpeaker;
 
 function onCreatePost()
 {
+    if (PlayState.SONG.stage == "phillyStreets" || PlayState.SONG.stage == "phillyBlazin") return;
+
     var curLevel = Paths.currentLevel;
 
     Paths.setCurrentLevel('weekend1');
@@ -11,20 +13,44 @@ function onCreatePost()
     abot.x -= 80;
     abot.y -= 200;
     abot.antialiasing = ClientPrefs.data.antialiasing;
+    for (i in [abot.bg, abot.eyeBg, abot.eyes, abot.speaker]) {
+        if (i == abot.bg) continue;
+        i.shader = game.gf.shader;
+    }
+    for (i in abot.vizSprites) i.shader = game.gf.shader;
     Paths.setCurrentLevel(curLevel);
 
     updateABotEye("dad", true);
     game.addBehindGF(abot);
+    game.setOnScripts("abot", abot);
 }
 
-function onSongStart() abot.snd = FlxG.sound.music;
+function onUpdatePost(e)
+{
+    if (PlayState.SONG.stage == "phillyStreets" || PlayState.SONG.stage == "phillyBlazin") return;
 
-function onMoveCamera(who) updateABotEye(who, false);
+    abot.alpha = game.gf.alpha;
+    for (i in [abot.bg, abot.eyeBg, abot.eyes, abot.speaker]) {
+        if (i == abot.bg) continue;
+        i.color = game.gf.color;
+    }
+}
+
+function onSongStart() {
+    if (PlayState.SONG.stage == "phillyStreets" || PlayState.SONG.stage == "phillyBlazin") return;
+    abot.snd = FlxG.sound.music;
+}
+
+function onMoveCamera(who) {
+    if (PlayState.SONG.stage == "phillyStreets" || PlayState.SONG.stage == "phillyBlazin") return;
+    updateABotEye(who, false);
+}
 
 function updateABotEye(who:String = "dad", finishInstantly:Bool = false)
 {
+    if (PlayState.SONG.stage == "phillyStreets" || PlayState.SONG.stage == "phillyBlazin") return;
+    
     if (who == "dad") abot.lookLeft(); else abot.lookRight();
 
     if (finishInstantly) abot.eyes.anim.curFrame = abot.eyes.anim.length - 1;
-    // script from psych engine discord, unchanged. again, all credit to DaPootisBird for this.
 }
